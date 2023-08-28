6d43b4e84761a0fdba6bac34a0a7ad344d09683c
Matthew Petro <matthew.petro@aa.com>
2023-08-28 11:53:18 -0700

    Added CssBaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CssBaseline } from '@mui/material'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 
@@ -8,6 +9,7 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
