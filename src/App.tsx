72649ab7ba35ed86b053e04c16d42d14a05e1808
Matthew Petro <matthew.petro@aa.com>
2023-09-07 07:32:47 -0700

    Removed grouping

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { useMemo, useState } from 'react'
+import { useState } from 'react'
 import { Paper } from '@mui/material'
 import useIrrigationEvents from './hooks/useIrrigationEvents'
 import {
   Resource,
   ViewState,
-  GroupingState,
-  IntegratedGrouping,
 } from '@devexpress/dx-react-scheduler'
 import {
   Scheduler,
@@ -18,7 +16,6 @@ import {
   ViewSwitcher,
   AppointmentTooltip,
   Resources,
-  GroupingPanel,
 } from '@devexpress/dx-react-scheduler-material-ui'
 import { startOfWeek, endOfWeek, startOfDay, endOfDay } from 'date-fns'
 
@@ -46,46 +43,31 @@ const getEndDate = (date: Date, viewName: string): Date => {
 
 const resources: Resource[] = [
   {
-    fieldName: 'group',
-    title: 'Group',
+    fieldName: 'title',
+    title: 'Zone',
     instances: [
-      { id: 'Lawn', text: 'Lawn' },
-      { id: 'Front yard', text: 'Front yard' },
-      { id: 'Back yard', text: 'Back yard' },
-      { id: 'New trees', text: 'New trees' },
+      { id: 'North lawn', text: 'North lawn' },
+      { id: 'South lawn', text: 'South lawn'},
+      { id: 'Front plants', text: 'Front plants' },
+      { id: 'Front trees', text: 'Front trees' },
+      { id: 'Back plants', text: 'Back plants' },
+      { id: 'Back trees', text: 'Back trees' },
+      { id: 'Tangelo tree', text: 'Tangelo tree' },
     ],
   },
 ]
 
-const idToGroupMap: {[key: number]: string }= {
-  767: 'Lawn',
-  768: 'Lawn',
-  676: 'Front yard',
-  677: 'Back yard',
-  769: 'Back yard',
-  675: 'New trees',
-  813: 'New trees',
-}
-
 function App() {
   const [viewCurrentDate, setViewCurrentDate] = useState<Date>(new Date())
-  const [currentViewName, setCurrentViewName] = useState<string>('Day')
+  const [currentViewName, setCurrentViewName] = useState<string>('Week')
   const { data: irrigationEvents } = useIrrigationEvents(
     getStartDate(viewCurrentDate, currentViewName),
     getEndDate(viewCurrentDate, currentViewName)
   )
-  const irrigationEventsWithResources = useMemo(
-    () =>
-      irrigationEvents?.map((event) => ({
-        ...event,
-        group: idToGroupMap[event.deviceId] ?? '',
-      })),
-    [irrigationEvents]
-  )
 
   return (
     <Paper>
-      <Scheduler height={800} data={irrigationEventsWithResources}>
+      <Scheduler height={800} data={irrigationEvents}>
         <ViewState
           currentDate={viewCurrentDate}
           onCurrentDateChange={setViewCurrentDate}
@@ -96,14 +78,11 @@ function App() {
         <DateNavigator />
         <TodayButton />
         <ViewSwitcher />
-        <GroupingState groupByDate={() => true} grouping={[{ resourceName: 'group' }]} />
         <DayView startDayHour={4} endDayHour={19} />
         <WeekView startDayHour={4} endDayHour={19} />
         <Appointments />
         <AppointmentTooltip showCloseButton />
         <Resources data={resources} />
-        <IntegratedGrouping />
-        <GroupingPanel />
       </Scheduler>
     </Paper>
   )
